420a3faa68816899c81c2188b27e97a0468a3600
MonikaBhardwaj18 <monikabhardwaj1896@gmail.com>
2019-04-17 11:44:15 -0400

    slides

---
+++ sql2.sql
@@ -0,0 +1,13 @@
+use mysqljdbc;
+
+
+ create table student(
+ rollno varchar(10),
+ firstname varchar(20),
+ lastname varchar(20),
+ dept varchar(15));
+ 
+ 
+ insert into student values('111','Raj','gs','Technical')
+ insert into student values('222','John','Mike','Sales')
+ insert into student values('333','Navin','Jessy','Sales')
